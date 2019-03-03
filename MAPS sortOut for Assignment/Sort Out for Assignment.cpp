69c2b3b01efe2e9fa1ff88894d9e3917ebbcdfe5
Josh <josh.windsor.mail@gmail.com>
2019-03-03 19:42:23 +0000

    name changes and starting SIMD

--- MAPS sortOut for Assignment/Sort Out for Assignment.cpp
+++ MAPS sortOut for Assignment/Sort Out for Assignment.cpp
@@ -27,12 +27,15 @@ Joshua Windsor
 #include "hr_time.h"		//for stopwatches
 #include <stdio.h>			//for fputs
 #include <omp.h>
+#include <immintrin.h>
 
 using namespace std;
 
+//2000 * 1000
 #define MAX_ROWS 2000
 #define MAX_COLS 1000
 #define MAX_CHARS 6			// numbers are in the range 1- 32,767, so 6 digits is enough.
+#define ASCII 0x30	// add this to a single digit 0-9 to make an ASCII character
 
 #define SortedRows "SortedRows.txt"
 #define SortedAll  "SortedAll.txt"		// for future use
@@ -53,20 +56,22 @@ void sortEachRow(void);
 void displayCheckData(void);
 void outputTimes(void);
 void outputDataAsString(void);
-void merge(int array[], int low, int mid, int high);
-void mergesort(int array[], int low, int high);
-
+void merge(int iArray[], int iBeginning, int iMiddle, int iEnd);
+void mergesort(int iArray[], int iBeginning, int iEnd);
+void ItoA_SIMD(int *num, char * numStr);
 
 int main(void)
 {
 	getData();
 
+	cout << "Sorting data...";
 	s1.startTimer();
 	sortEachRow();
 	s1.stopTimer();
 
 	displayCheckData();
 
+	cout << "\n\nOutputting data to " << SortedRows << "...";
 	s2.startTimer();
 	outputDataAsString();
 	s2.stopTimer();
@@ -93,23 +98,9 @@ void getData()		// Generate the same sequence of 'random' numbers.
 //*********************************************************************************
 void sortEachRow()
 {
-	cout << "Sorting data...";
-	//
 	for(int i=0; i<MAX_ROWS; i++)
-	{	
+	{
 		mergesort(_data[i], 0, MAX_COLS - 1);
-		//Use a bubble sort on a row 
-		//for(int n=MAX_COLS-1; n>=0; n--)
-		//{   for(int j=0; j<n; j++)
-		//	{
-		//		if(_data[i][j] > _data[i][j+1])
-		//		{
-		//			int temp = _data[i][j];
-		//			_data[i][j] = _data[i][j+1];
-		//			_data[i][j+1] = temp;
-		//		}
-		//	}
-		//}
 	}
 }
 
@@ -147,13 +138,13 @@ void outputTimes()
 
 void outputDataAsString()
 {
-	char numString[MAX_CHARS];
+	char numString[MAX_CHARS * 8];
 	string odata;
-	cout << "\n\nOutputting data to " << SortedRows << "...";
 
+	#pragma omp parallel for reduction(+:odata)
 	for (int i = 0; i<MAX_ROWS; i++){
-		for (int j = 0; j<MAX_COLS; j++){
-			_itoa_s<6>(_data[i][j], numString, 10);
+		for (int j = 0; j<MAX_COLS; j+=8){
+			ItoA_SIMD(&_data[i][j], numString);
 			odata += numString;
 			odata += "\t";
 		}
@@ -166,68 +157,135 @@ void outputDataAsString()
 	fclose(sodata);
 }
 
+
 //Ref: https://www.comrevo.com/2016/02/openmp-program-for-merge-sort.html
 //TODO: SIMD
 
-inline void merge(int array[], int low, int mid, int high)
+inline void merge(int iArray[], int iBeginning, int iMiddle, int iEnd)
 {
-	int i, j, k, m;
-	j = low;
-	m = mid + 1;
-	for (i = low; j <= mid && m <= high; i++)
+	int a,
+		b = iBeginning,
+		c,
+		d = iMiddle + 1;
+
+	for (a = iBeginning; b <= iMiddle && d <= iEnd; a++)
 	{
-		if (array[j] <= array[m])
+		if (iArray[b] <= iArray[d])
 		{
-			_temp[i] = array[j];
-			j++;
+			_temp[a] = iArray[b];
+			b++;
 		}
 		else
 		{
-			_temp[i] = array[m];
-			m++;
+			_temp[a] = iArray[d];
+			d++;
 		}
 	}
-	if (j > mid)
+	if (b > iMiddle)
 	{
-		for (k = m; k <= high; k++)
+		for (c = d; c <= iEnd; c++)
 		{
-			_temp[i] = array[k];
-			i++;
+			_temp[a] = iArray[c];
+			a++;
 		}
 	}
 	else
 	{
-		for (k = j; k <= mid; k++)
+		for (c = b; c <= iMiddle; c++)
 		{
-			_temp[i] = array[k];
-			i++;
+			_temp[a] = iArray[c];
+			a++;
 		}
 	}
-	for (k = low; k <= high; k++)
+	for (c = iBeginning; c <= iEnd; c++)
 	{
-		array[k] = _temp[k];
+		iArray[c] = _temp[c];
 	}
 }
 
 
-inline void mergesort(int array[], int low, int high)
+inline void mergesort(int iArray[], int iBeginning, int iEnd)
 {
-	if (low < high)
+	if (iBeginning < iEnd)
 	{
-		const int mid = (low + high) / 2;
+		int middle = (iBeginning + iEnd) / 2;
 
 		#pragma omp parallel sections num_threads(2)
 		{
 			#pragma omp section
 			{
-				mergesort(array, low, mid);
+				mergesort(iArray, iBeginning, middle);
 			}
 
 			#pragma omp section
 			{
-				mergesort(array, mid + 1, high);
+				mergesort(iArray, middle + 1, iEnd);
 			}
 		}
-		merge(array, low, mid, high);
+		merge(iArray, iBeginning, middle, iEnd);
 	}
+
 }
+
+//Ref: https://github.com/SeanShortreed/ParallelProgramming/blob/22d4a97ccc6494ea8234f6f7a5005885b4e0df81/shortreed_sean_SIMD.cpp
+//TODO: Make unique, rename stuff and fix up
+
+
+inline void ItoA_SIMD(int *num, char * numStr)
+{
+	_declspec(align(16)) short DataToSort[8] = { num[0], num[1],  num[2],  num[3], num[4], num[5], num[6], num[7] };
+
+	//Simd vars
+	__m128i* OrigData = (__m128i*)DataToSort;
+	__m128i SimdData;
+	__m128i opNumber;
+	__m128i DataToUse = *OrigData;
+	__m128i SavedData;
+
+	//start for loop
+	for (int i = 0; i < 5; i++)
+	{
+		SimdData = DataToUse;
+		SavedData = SimdData;
+		//setup opNumber with magic number
+		opNumber = _mm_set1_epi16(0x6667);
+		//times by magic number
+		SimdData = _mm_mulhi_epi16(opNumber, SimdData);
+		//shift right
+		SimdData = _mm_srai_epi16(SimdData, 2);
+		//Save these numbers for next round
+		DataToUse = SimdData;
+		//setup opNumber with 10
+		opNumber = _mm_set1_epi16(10);
+		//times by 10
+		SimdData = _mm_mullo_epi16(opNumber, SimdData);
+		//take new num from orig num
+		SimdData = _mm_sub_epi16(SavedData, SimdData);
+		//SimdData now contains single digits
+		//Convert SimdData to char numbers
+		//Setup opNumber with char conversion
+		opNumber = _mm_set1_epi16(ASCII);
+		//do the addition
+		SimdData = _mm_add_epi16(opNumber, SimdData);
+		//SimdData now contains correct chars
+		//sort letters into correct arrays
+		numStr[4 - i] = SimdData.m128i_i16[0];
+		numStr[6 + 4 - i] = SimdData.m128i_i16[1];
+		numStr[12 + 4 - i] = SimdData.m128i_i16[2];
+		numStr[18 + 4 - i] = SimdData.m128i_i16[3];
+		numStr[24 + 4 - i] = SimdData.m128i_i16[4];
+		numStr[30 + 4 - i] = SimdData.m128i_i16[5];
+		numStr[36 + 4 - i] = SimdData.m128i_i16[6];
+		numStr[42 + 4 - i] = SimdData.m128i_i16[7];
+	}
+
+	numStr[5] = '\t';
+	numStr[6 + 5] = '\t';
+	numStr[12 + 5] = '\t';
+	numStr[18 + 5] = '\t';
+	numStr[24 + 5] = '\t';
+	numStr[30 + 5] = '\t';
+	numStr[36 + 5] = '\t';
+	numStr[42 + 5] = '\0';
+}
+//***********************************************************
